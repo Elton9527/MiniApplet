1ec4fcbc40ad3426812e3bfbc71cebbb680eea2b
Elton <wgaobo@sina.cn>
2018-05-24 11:59:50 +0800

    小程序生成二维码DEMO

---
+++ miniAppletQrcode.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Curl 处理请求，忽略 http https
+ * @param $url
+ * @return mixed
+ * @author wanggaobo
+ */
+public function http_curl($url){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);//这个是重点。
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
+
+/*
+ * 获取TokenAccess
+ */
+public function getTokenAccess(){
+    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$this->appid}&secret={$this->secret}";
+    $res = $this->http_curl($url);
+    $result = json_decode($res, true);
+    $access_token = $result["access_token"];
+    return $access_token;
+}
+
+
+/**
+ * FN:生成永久二维码
+ */
+public function createMiniPermanentQrCode(){
+    $token = $this->getTokenAccess();
+    $input = $this->input->get();
+    //生成小程序二维码 接口A：适用于需要的码数量较少的业务场景
+    $api = "https://api.weixin.qq.com/wxa/getwxacode?access_token={$token}";
+    //生成小程序二维码 接口B：适用于需要的码数量极多的业务场景
+    //$api = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token={$token}";
+    //生成小程序二维码 接口C：适用于需要的码数量较少的业务场景
+    //$api = "https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token={$token}";
+    $params = [
+        //'scene' => $input['scene'],
+        'path' => 'pages/XX/XX',
+    ];
+    $response_obj = $this->curl->post($api, json_encode($params));
+    $data='image/png;base64,'.base64_encode($response_obj);
+    echo '<img src="data:'.$data.'">';
+}
